175424ce1357dbca62459e70d601e526fec7525c
warley.henrique <warley.henrique93@gmail.com>
2020-01-22 23:21:25 -0300

    load for devs api

--- src/App.js
+++ src/App.js
@@ -1,85 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 import './global.css';
 import './app.css';
 import './aside.css';
 import './main.css';
+import api from './services/api';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 
 
 function App() {
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition
-  },[])
+  const [devs, setDevs] = useState([]);
 
+  
+  useEffect(()=>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  },[]);
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs',data);
+ 
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="">Usuario do Github</label>
-            <input name="github_username" id="username_github"></input>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="">Tecnologias</label>
-            <input name="techs" id="techs"></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/26408030?s=460&v=4" alt="" />
-              <div className="user-info">
-                <strong>Warley</strong>
-                <span>Node, React, React Native, Vue</span>
-              </div>
-            </header>
-            <p>Estudante de Sistemas de Informação e um apaixonado por Tecnologia</p>
-            <a href="https://github.com/warleyhenrique">Acessar perfil do github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/26408030?s=460&v=4" alt="" />
-              <div className="user-info">
-                <strong>Warley</strong>
-                <span>Node, React, React Native, Vue</span>
-              </div>
-            </header>
-            <p>Estudante de Sistemas de Informação e um apaixonado por Tecnologia</p>
-            <a href="https://github.com/warleyhenrique">Acessar perfil do github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/26408030?s=460&v=4" alt="" />
-              <div className="user-info">
-                <strong>Warley</strong>
-                <span>Node, React, React Native, Vue</span>
-              </div>
-            </header>
-            <p>Estudante de Sistemas de Informação e um apaixonado por Tecnologia</p>
-            <a href="https://github.com/warleyhenrique">Acessar perfil do github</a>
-          </li>
+          {devs.map(dev=>(
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
